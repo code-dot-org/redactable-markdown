fbdef2bf7f007667cfaeeb830979589de3684b74
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-04-06 17:38:10 -0700

    add shared method for rendering source content to a redacted MDAST

--- src/cdoFlavoredParser.js
+++ src/cdoFlavoredParser.js
@@ -10,9 +10,9 @@ const redactedLink = require('./plugins/redactedLink');
 const tiplink = require('./plugins/tiplink');
 
 module.exports = class CdoFlavoredParser {
-  static getPlugins = function () {
+  static getPlugins = function() {
     return [redactedLink, tiplink];
-  }
+  };
 
   static getParser = function() {
     return unified().use(parse).use(this.getPlugins());
@@ -22,20 +22,26 @@ module.exports = class CdoFlavoredParser {
     return this.getParser().use(html).processSync(source).contents;
   };
 
+  static sourceToRedactedMdast = function(source) {
+    return this.getParser().use({ settings: { redact: true } }).parse(source);
+  };
+
   static sourceToRedacted = function(source) {
+    const sourceTree = this.sourceToRedactedMdast(source);
     return this.getParser()
-      .use({ settings: { redact: true } })
       .use(stringify)
       .use(renderRedactions)
-      .processSync(source).contents;
+      .stringify(sourceTree);
   };
 
   static sourceAndRedactedToMergedMdast = function(source, redacted) {
-    const sourceTree = this.getParser().use({ settings: { redact: true } }).parse(source);
-    const redactedTree = this.getParser().use(restoreRedactions(sourceTree)).parse(redacted);
+    const sourceTree = this.sourceToRedactedMdast(source);
+    const redactedTree = this.getParser()
+      .use(restoreRedactions(sourceTree))
+      .parse(redacted);
 
     return redactedTree;
-  }
+  };
 
   static sourceAndRedactedToHtml = function(source, redacted) {
     const mergedMdast = this.sourceAndRedactedToMergedMdast(source, redacted);
