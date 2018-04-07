fd89b38977c52861e03ed0e612c279c76ccec34e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-04-06 17:00:37 -0700

    add more overview comments

--- src/plugins/redactedLink.js
+++ src/plugins/redactedLink.js
@@ -1,6 +1,20 @@
 let redact;
 let tokenizeLink;
 
+/**
+ * Parser extension to support rendering of links (and images) when in redact
+ * mode.
+ *
+ * Note that most plugins that support redact mode are adding the ability to
+ * parse an entirely new syntax in both normal and redact mode, whereas this one
+ * is _extending_ the build-in ability to parse links (and image) to add redact
+ * mode.
+ *
+ * As such, this basically acts as an interstitial between the build-in image
+ * method to turn its output into a redaction when appropriate.
+ *
+ * @see https://github.com/remarkjs/remark/tree/remark-parse%405.0.0/packages/remark-parse#extending-the-parser
+ */
 module.exports = function redactedLink() {
   const Parser = this.Parser;
   const tokenizers = Parser.prototype.inlineTokenizers;
