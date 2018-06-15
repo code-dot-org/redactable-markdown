caa7be9849d945328ae38b7d95f2fe447e5614fc
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-15 11:47:18 -0700

    add some input/output util methods

---
+++ src/utils/io.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+module.exports.readFromFileOrStdin = function (path, callback) {
+  const readStream = path ? fs.createReadStream(path) : process.stdin;
+  let inputData = "";
+
+  readStream
+    .setEncoding('utf8')
+    .on('readable', () => {
+      const chunk = readStream.read();
+      if (chunk !== null) {
+        inputData += chunk;
+      }
+    })
+    .on('end', () => {
+      callback(inputData);
+    });
+}
+
+module.exports.parseAsSerialized = function (input) {
+  try {
+    return JSON.parse(input);
+  } catch (e) {
+    // input is not valid JSON
+  }
+
+  return input.toString();
+}
