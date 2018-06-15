ccf207e2bc48b13534e55cc62edbcbae3b055763
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-15 11:47:57 -0700

    update redaction to work with either file or stdin input

--- src/bin/redact.js
+++ src/bin/redact.js
@@ -1,27 +1,38 @@
-const parser = require('../cdoFlavoredParser');
-const parseArgs = require('minimist')
 const fs = require('fs');
+const parseArgs = require('minimist')
+
+const ioUtils = require('../utils/io');
+const parser = require('../cdoFlavoredParser');
 
 const argv = parseArgs(process.argv.slice(2));
 
 const helpFlag = (argv.h || argv.help);
 
 if (helpFlag) {
-  process.stdout.write("usage: redact INPUTFILE\n");
+  process.stdout.write("usage: redact [INFILE] [options]\n");
+  process.stdout.write("\n");
+  process.stdout.write("Reads content from INFILE if specified, STDIN otherwise.\n");
+  process.stdout.write("Content can be plain text or JSON.\n");
+  process.stdout.write("Content will be redacted. If the content is JSON, all string values (including those several levels deep) will be redacted.\n");
+  process.stdout.write("\n");
   process.stdout.write("options:\n");
   process.stdout.write("\t-h, --help: print this help message\n");
   process.stdout.write("\t-o OUTFILE: output to OUTFILE rather than stdout\n");
   process.exit()
 }
 
-const inputFile = argv._[0];
-let inputData = fs.readFileSync(inputFile);
-try {
-  inputData = JSON.parse(inputData);
-} catch (e) {
-  inputData = inputData.toString();
-}
-const outputFile = argv.o;
+ioUtils.readFromFileOrStdin(argv._[0], (inputData) => {
+  inputData = ioUtils.parseAsSerialized(inputData);
+  const outputData = redact(inputData);
+  const formattedOutput = typeof outputData === "object" ? JSON.stringify(outputData, null, 2) : outputData;
+
+  const outputFile = argv.o;
+  if (outputFile) {
+    fs.writeFileSync(outputFile, formattedOutput);
+  } else {
+    process.stdout.write(formattedOutput)
+  }
+});
 
 function redact(data) {
   if (typeof data === "string") {
@@ -37,13 +48,4 @@ function redact(data) {
   } else {
     throw Error('cannot process content of type ' + typeof data);
   }
-}
-
-const outputData = redact(inputData);
-const formattedOutput = typeof outputData === "object" ? JSON.stringify(outputData, null, 2) : outputData;
-
-if (outputFile) {
-  fs.writeFileSync(outputFile, formattedOutput);
-} else {
-  process.stdout.write(formattedOutput)
 }
