0e4a194972e14fca9683caef5aa8acbd39ca02c5
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-21 14:06:41 -0700

    Add support for serializing to and deserializing from yaml
    Also rename the serialization and deserialization helpers to be more
    clear

--- src/utils/io.js
+++ src/utils/io.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 module.exports.readFromFileOrStdin = function (path) {
   const readStream = path ? fs.createReadStream(path) : process.stdin;
@@ -28,25 +29,36 @@ module.exports.writeToFileOrStdout = function (path, data) {
   }
 }
 
-module.exports.formatAsSerialized = function (output, format) {
+module.exports.serialize = function (format, output) {
   if (!format) {
     format = (typeof output === "object") ? 'json' : 'txt';
   }
 
   if (format === 'json') {
     return JSON.stringify(output, null, 2);
+  } else if (format === 'yaml') {
+    return yaml.safeDump(output, {
+      noRefs: true,
+      lineWidth: -1
+    });
   } else if (format === 'txt') {
     return output.toString();
   } else {
-    throw Error("do not know how to output to format " + format);
+    throw Error("do not know how to serialize to format " + format);
   }
 }
 
-module.exports.parseAsSerialized = function (input) {
+module.exports.deserialize = function (input) {
   try {
     return JSON.parse(input);
   } catch (e) {
     // input is not valid JSON
+  }
+
+  try {
+    return yaml.safeLoad(input);
+  } catch (e) {
+    // input is not valid YAML
   }
 
   return input.toString();
